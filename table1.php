0801d9cee640eba8c530d2c4749b1e8797d1f147
netniyom999 <76762087+netniyom999@users.noreply.github.com>
2021-02-10 09:53:29 +0700

    Add files via upload

--- table1.php
+++ table1.php
@@ -1,8 +1,24 @@
 <?php
     require_once("dbcon.php");
     $sql = "SELECT * FROM movie";
+
+    if(isset($_GET['search_click']))
+    {
+      $sql = "SELECT * FROM movie WHERE movie_name LIKE '%{$_GET['search']}%'";
+      echo "<p>คุณกำลังค้นหา : {$_GET['search']}</p>";
+    ?>
+    <a href=".">ย้อนกลับ</a></small>
+    <?php 
+    } 
     $result = $conn->query($sql); 
 ?>
+
+
+<form action="" method="get">
+  <label for="search">ช่องค้นหา</label>
+  <input type="text" name="search" id="search" placeholder="ช่องค้นหา...">
+  <button type="submit" name="search_click">ค้นหา</button>
+</form>
 <a href="insert_from.php">เพิ่มภาพยนต์</a>
 <table style="width:100%" border = "1">
   <tr>
