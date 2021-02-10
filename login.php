1deab39b5b73d9ab236d651a8e506bfb13ca836f
netniyom999 <76762087+netniyom999@users.noreply.github.com>
2021-02-10 10:09:24 +0700

    Add files via upload

---
+++ login.php
@@ -0,0 +1,11 @@
+<h2>เข้าสู่ระบบ</h2>
+<form action="." method="post">
+    <label for="movie_name">ชื่อผู้ใช้งาน</label>
+    <input type="text" name="movie_cname" id="movie_cname">
+    <br><br>
+    <label for="pin">PIN</label>
+    <input type="text" name="pin" id="pin">
+    <br><br>
+    <button type="submit" name="login">เข้าสู่ระบบ</button>
+    <button type="reset">ล้างข้อมูล</button>
+</form>
